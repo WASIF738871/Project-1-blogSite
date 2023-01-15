2d5b5017d4d2e48cd4e1b4d4f04ccfc220cab10b
WASIF738871 <erwasifmohd786@gmail.com>
2023-01-15 09:25:03 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const route = require('./routes/route.js');
+require('dotenv').config();
+let {PORT, MongoDB_CLUSTER} = process.env;
+
 const app =express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
 
-
-mongoose.connect("mongodb+srv://jai84322:Bing%401234%23@demo.3li78.mongodb.net/project1-blog?retryWrites=true&w=majority"
-,{useNewUrlParser: true})
+mongoose.connect(MongoDB_CLUSTER)
 .then(()=>console.log('MongoDB is connected'))
 .catch(err => console.log(err));
 
 app.use('/',route);
-app.listen(process.env.PORT || 3000, function(){
-    console.log('express app running on PORT '+(process.env.PORT || 3000))
+
+app.listen(PORT, ()=>{
+    console.log(`server is running at http://localhost:${PORT}`)
 });
